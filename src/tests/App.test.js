853ed38cc7a505f36840d15355d40eb1bcd44761
Izabel de Barros <izabelfeigel@gmail.com>
2022-08-22 18:51:28 -0300

    Tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,80 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
+import testData from '../../cypress/mocks/testData';
+import SWProvider from '../context/SWProvider';
 
-test('I am your test', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello, App!/i);
-  expect(linkElement).toBeInTheDocument();
+beforeEach(() => {
+  jest.spyOn(global, 'fetch').mockImplementationOnce(() =>
+      Promise.resolve({
+          json: () => Promise.resolve(testData),
+      })
+  )
+});
+
+afterEach(() => {
+  jest.resetAllMocks();
+});
+
+test('If the API is being called', async () => {
+ render(<SWProvider><App /></SWProvider>)
+ await waitFor(() => expect(fetch).toHaveBeenCalled());
+ await waitFor(() => expect(fetch).toHaveBeenCalledTimes(1));
+ await waitFor(() => expect(fetch).toHaveBeenCalledWith('https://swapi-trybe.herokuapp.com/api/planets/'));
+});
+
+test('If filters are present', () => {
+  render(<SWProvider><App /></SWProvider>)
+  const filterByName = screen.getByTestId('name-filter');
+  const columnFilter = screen.getByTestId('column-filter');
+  const comparisonFilter = screen.getByTestId('comparison-filter');
+  const valueFilter = screen.getByTestId('value-filter');
+
+  expect(filterByName).toBeInTheDocument();
+  expect(columnFilter).toBeInTheDocument();
+  expect(comparisonFilter).toBeInTheDocument();
+  expect(valueFilter).toBeInTheDocument();
+});
+
+test('the buttons are present', () => {
+  render(<SWProvider><App /></SWProvider>)
+  const filterBtn = screen.getByTestId('button-filter');
+  const deleteBtn = screen.getByTestId('button-remove-filters');
+  
+  expect(filterBtn).toBeDefined();
+  expect(deleteBtn).toBeDefined();
+});
+
+test('number simulation', () => {
+  render(<SWProvider><App /></SWProvider>)
+
+  const columnFilter = screen.getByTestId('column-filter');
+  const comparisonFilter = screen.getByTestId('comparison-filter');
+  const valueFilter = screen.getByTestId('value-filter');
+  const filterBtn = screen.getByTestId('button-filter');
+  const deleteBtn = screen.getByTestId('button-remove-filters');
+
+  userEvent.selectOptions(columnFilter, 'population');
+  userEvent.selectOptions(comparisonFilter, 'maior que');
+  userEvent.type(valueFilter, '1000');
+  userEvent.click(filterBtn);
+
+  expect(screen.getByTestId('filter')).toBeInTheDocument();
+
+  userEvent.selectOptions(columnFilter, 'diameter');
+  userEvent.selectOptions(comparisonFilter, 'maior que');
+  userEvent.type(valueFilter, '1000');
+  userEvent.click(filterBtn);
+});
+
+test('name simulation', () => {
+  render(<SWProvider><App /></SWProvider>)
+
+  const filterByName = screen.getByTestId('name-filter');
+  const ooPlanet = 'Tatooine';
+
+  userEvent.type(filterByName, 'o');
+
+  expect(screen.getByRole('cell', { name: /Hoth/i })).toBeDefined();
 });
